60dbbd16a1f1b1be0fb2e965344bb464e1ae834c
nnkrbyok-kxxrin <kxxxx.mxxxkx@gmail.com>
2021-02-01 13:24:57 +0000

    完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+
   def top
   end
 
@@ -38,8 +39,10 @@ class BooksController < ApplicationController
 
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    if book.destroy
+      flash[:notice] = "Book was successfully destroyed."
+      redirect_to books_path
+    end
   end
 
   private
